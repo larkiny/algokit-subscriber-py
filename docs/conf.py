aec8c2162420604e074ca3db203049a2d3596ef7
Larkin Young <larkin.young@algorand.foundation>
2026-01-14 13:33:09 +0000

    Fixed duplicate output error

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "api"]
 
 nitpick_ignore = [("py:class", "optional")]
 intersphinx_mapping = {
